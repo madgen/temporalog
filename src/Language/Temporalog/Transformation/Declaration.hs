c4928ef66bacb63d44ebdd5d3241b923bbfd70f1
Mistral Contrastin <me@madgen.net>
2019-01-07 23:00:19 +0000

    Complete function to remove declarations

--- src/Language/Temporalog/Transformation/Declaration.hs
+++ src/Language/Temporalog/Transformation/Declaration.hs
@@ -1,11 +1,18 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Language.Temporalog.Transformation.Declaration where
 
 import Protolude
 
 import qualified Language.Vanillalog.Generic.Logger as Log
-import qualified Language.Vanillalog.Generic.AST as GA
+import qualified Language.Vanillalog.Generic.AST as AG
 
 import Language.Temporalog.AST
 
-removeDecls :: Program -> Log.LoggerM (GA.Program Void Op)
-removeDecls = _
+removeDecls :: Program -> AG.Program Void Op
+removeDecls AG.Program{..} = AG.Program{_statements = newStatements,..}
+  where
+  newStatements :: [ AG.Statement Void Op ]
+  newStatements = map (\AG.StSentence{..} -> AG.StSentence{..})
+                . filter (\case {AG.StSentence{} -> True; _ -> False})
+                $ _statements
