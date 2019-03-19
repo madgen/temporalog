a82c3edea17bed2a96979237f16496b4d84148ab
Mistral Contrastin <me@madgen.net>
2019-03-19 23:20:46 +0000

    Monadic actions for fresh variables and time environment

--- src/Language/Temporalog/Transformation/Temporal/Compiler.hs
+++ src/Language/Temporalog/Transformation/Temporal/Compiler.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedLabels #-}
 
 module Language.Temporalog.Transformation.Temporal.Compiler
   ( eliminateTemporal
@@ -10,14 +11,14 @@ import Protolude
 
 import Control.Arrow ((>>>))
 
-import Data.Functor.Foldable (Base, cata, embed)
-import Data.Text (pack)
+import           Data.Functor.Foldable (Base, cata, embed)
+import           Data.Text (pack)
+import qualified Data.Map.Strict as M
 
-import qualified Language.Vanillalog.AST as A
 import           Language.Vanillalog.Generic.Transformation.Util (Algebra)
 import qualified Language.Vanillalog.Generic.AST as AG
 import qualified Language.Vanillalog.Generic.Logger as Log
-import           Language.Vanillalog.Generic.Parser.SrcLoc (span)
+import           Language.Vanillalog.Generic.Parser.SrcLoc (span, dummySpan)
 
 import           Language.Temporalog.AST
 import qualified Language.Temporalog.Metadata as MD
@@ -42,6 +43,41 @@ freshPredSym = do
   where
     go predSyms i | candidate <- PredicateSymbol [ "aux" <> pack (show i) ] =
       if candidate `elem` predSyms then go predSyms (i + 1) else candidate
+
+type TimeEnv = M.Map AG.PredicateSymbol Term
+type TimeEnvMT = ReaderT TimeEnv
+
+runTimeEnvMT :: Monad m => TimeEnvMT m a -> TimeEnv -> m a
+runTimeEnvMT = runReaderT
+
+setClock :: Monad m
+         => AG.PredicateSymbol -> Term -> TimeEnvMT m a -> TimeEnvMT m a
+setClock predSym time = local (M.insert predSym time)
+
+type FreshVarMT = StateT ([ Var ], Int)
+
+runFreshVarMT :: Monad m => [ Var ] -> FreshVarMT m a -> m a
+runFreshVarMT vars = (`evalStateT` (vars, 0))
+
+freshVar :: Monad m => FreshVarMT m Var
+freshVar = do
+  (vars, ix) <- get
+  put (vars, ix + 1)
+  if var ix `elem` vars
+    then freshVar
+    else pure $ var ix
+  where
+  var ix = Var dummySpan ("X" <> pack (show ix))
+
+freshTimeEnv :: MD.Metadata -> AG.Clause b c -> FreshVarMT Log.LoggerM TimeEnv
+freshTimeEnv metadata cl@AG.Clause{..} = do
+  timePredSyms <- lift timePredSymsM
+  freshVars <- fmap TVar <$> replicateM (length timePredSyms) freshVar
+  pure $ M.fromList $ zip timePredSyms freshVars
+  where
+  predSyms = map #_predSym $ AG.atoms _head <> AG.atoms _body
+  timePredSymsM = concatMap MD.timingPreds
+             <$> traverse (`MD.lookupM` metadata) predSyms
 
 -- |Assembles a clause
 mkClause :: PredicateSymbol              -- |Name of the predicate to define
