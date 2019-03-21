5c59b86e94dbe3cb28f6157eedaeca3321bfa504
Mistral Contrastin <me@madgen.net>
2019-03-21 12:54:15 +0000

    Change the identities used due to change in basis

--- src/Language/Temporalog/Transformation/Temporal/Identities.hs
+++ src/Language/Temporalog/Transformation/Temporal/Identities.hs
@@ -17,7 +17,7 @@ applyTemporalIdentities :: AG.Program Void HOp (BOp 'Temporal)
                         -> AG.Program Void HOp (BOp 'Temporal)
 applyTemporalIdentities = round2 . round1
 
--- |Eliminate AX, AG, AU
+-- |Eliminate AX, AG, EG, AU
 round1 :: AG.Program Void HOp (BOp 'Temporal)
        -> AG.Program Void HOp (BOp 'Temporal)
 round1 = transformBody (cata alg)
@@ -26,19 +26,19 @@ round1 = transformBody (cata alg)
                  (Subgoal (BOp 'Temporal) Term)
   alg (SAXF span timePredSym child) = SNeg span $ SEX span timePredSym $ SNeg span child
   alg (SAGF span timePredSym child) = SNeg span $ SEF span timePredSym $ SNeg span child
+  alg (SEGF span timePredSym child) = SNeg span $ SAF span timePredSym $ SNeg span child
   alg (SAUF span timePredSym child1 child2) =
     SConj span (SEU span timePredSym (SNeg span child2)
                                      (SConj span (SNeg span child1) (SNeg span child2)))
-               (SNeg span $ SEG span timePredSym $ SNeg span child2)
+               (SAF span timePredSym child2)
   alg s = embed s
 
--- |Eliminate AF, EF
+-- |Eliminate EF
 round2 :: AG.Program Void HOp (BOp 'Temporal)
        -> AG.Program Void HOp (BOp 'Temporal)
 round2 = transformBody (cata alg)
   where
   alg :: Algebra (Base (Subgoal (BOp 'Temporal) Term))
                  (Subgoal (BOp 'Temporal) Term)
-  alg (SAFF span timePredSym child) = SNeg span $ SEG span timePredSym $ SNeg span child
   alg (SEFF span timePredSym child) = SEU span timePredSym (SDogru span) child
   alg s = embed s
