309afe64408f07bbf9f3dc2c11c0dfd19092d8bd
Mistral Contrastin <me@madgen.net>
2019-03-26 23:51:55 +0000

    Change the identities to match the new base

--- src/Language/Temporalog/Transformation/Temporal/Identities.hs
+++ src/Language/Temporalog/Transformation/Temporal/Identities.hs
@@ -26,19 +26,19 @@ round1 = transformBody (cata alg)
                  (Subgoal (BOp 'Temporal) Term)
   alg (SAXF span timePredSym child) = SNeg span $ SEX span timePredSym $ SNeg span child
   alg (SAGF span timePredSym child) = SNeg span $ SEF span timePredSym $ SNeg span child
-  alg (SEGF span timePredSym child) = SNeg span $ SAF span timePredSym $ SNeg span child
   alg (SAUF span timePredSym child1 child2) =
     SConj span (SEU span timePredSym (SNeg span child2)
                                      (SConj span (SNeg span child1) (SNeg span child2)))
                (SAF span timePredSym child2)
   alg s = embed s
 
--- |Eliminate EF
+-- |Eliminate AF, EF
 round2 :: AG.Program Void HOp (BOp 'Temporal)
        -> AG.Program Void HOp (BOp 'Temporal)
 round2 = transformBody (cata alg)
   where
   alg :: Algebra (Base (Subgoal (BOp 'Temporal) Term))
                  (Subgoal (BOp 'Temporal) Term)
+  alg (SAFF span timePredSym child) = SNeg span $ SEG span timePredSym $ SNeg span child
   alg (SEFF span timePredSym child) = SEU span timePredSym (SDogru span) child
   alg s = embed s
